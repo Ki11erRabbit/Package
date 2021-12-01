c8ac0a8f92676a4e175deaf20c966074085bf110
ki11errabbit <unlikelytitan@gmail.com>
2021-12-01 14:04:10 -0700

    Getting rid of match statement using cmdlargs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ enum Commands {
     Update,
     Search,
     Set,
-    Pass:,
+    Pass,
     Initialize,
     Help,
 }
@@ -15,31 +15,36 @@ fn main() {
     let mut command = String::new();
     let mut option = String::new();
     let mut args = String::new();
-    let mut input = String::new();
+    let args: Vec<String> = env::args().collect();
 
     //TODO: change to use command line arguments
     io::stdin()
         .read_line(&mut input);
 }
 
-fn parse_input (input: String, command: &String, option: &String, args: &String) {
+fn parse_input (input: String, command: &mut String, option: &mut String, args: &mut String) {
     let vect: Vec<&str> = input.split(' ').collect();
     let mut foundCommand = false;
 
     for element in vect {
-
+        match_command(command, vect[1]);
     }
 }
 
-fn match_command (command: &String, input: String) -> bool {
-    match command {
-        Commands::Install.to_string() => {
-            command = Commands::Install.to_string();
+fn match_command (command: &mut String, input: &str) -> bool {
+
+    if input.eq("install") {
+        return true
+    }
+    /*match (*input) {
+        install => {
+            *command = String::from("install");
             true
         }
-        Commands::Remove.to_string() => {
-            command = Commands::Remove.to_string();
+        "remove" => {
+            *command = String::from("install");
+            true
         }
-    }
-
+    }*/
+    false
 }
